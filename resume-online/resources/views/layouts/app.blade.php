bcbe5db169f28c827a31abe909639005c83dc787
Poomdevkub <sorawit.3p@gmail.com>
2024-09-20 21:55:55 +0700

    เพิ่มหน้า type (การเลือกชนิดuser)

--- resume-online/resources/views/layouts/app.blade.php
+++ resume-online/resources/views/layouts/app.blade.php
@@ -13,9 +13,9 @@
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
-    <!-- Custom Login & Register CSS -->
+    <!-- Custom CSS -->
     <link href="{{ asset('auth.css') }}" rel="stylesheet">
-
+    
 
     <!-- Scripts -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
--- resume-online/resources/views/type.blade.php
+++ resume-online/resources/views/type.blade.php
@@ -5,8 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Type</title>
+    <link href="{{ asset('type.css') }}" rel="stylesheet">
 </head>
 <body>
-    <h1 style="text-align: center">ชนิด</h1>
+    <h1>ชนิด</h1>
+    <div class="container">
+        <div class="box">
+            <img src="./images/ผู้ที่กำลังมองหางาน.jpg  " alt="ผู้ที่กำลังมองหางาน">
+            <p>ผู้ที่กำลังมองหางาน</p>
+        </div>
+        <div class="box">
+            <img src="./images/บริษัท-ผู้จัดหางาน.jpg" alt="บริษัท, ผู้จัดหางาน">
+            <p>บริษัท, ผู้จัดหางาน</p>
+        </div>
+    </div>
 </body>
 </html>
