bdd2065b3de0bfc6640d16f1a8afb644de955b11
snaranja <153524049+snaranja@users.noreply.github.com>
2024-09-21 13:42:06 +0700

    มี navbar แล้วจ้าพี่จ๋า

--- resume-online/resources/views/findUser/findUser.blade.php
+++ resume-online/resources/views/findUser/findUser.blade.php
@@ -1,14 +1,39 @@
-@extends('layouts.app')
+@extends('layouts.company')
 
 @section('content')
-    <h1 class="mb-4">ผู้ใช้ทั้งหมด</h1>
-    <div class="row">
+    <h1 class="mb-4"></h1>
+
+    <div class="container-fluid"> <!--ค้นหาตำแหน่ง-->
+        <form class="d-flex" role="search" style="margin-top: 5%">
+            <input class="form-control me-2" type="search" placeholder="ค้นหาตำแหน่งงาน" aria-label="Search">
+            <button class="btn btn-primary" type="submit">ค้นหา</button>
+            <style>
+                form{
+                    width: 30%;
+                    margin-left: 70%;
+                }
+            </style>
+        </form>
+   </div>
+
+   <!--ค้นหาจังหวัด-->
+   <select class="form-select" aria-label="Default select example" style="margin-top: 2%; width: 30%; margin-left:69.15%;">
+    <option selected>เลือกจังหวัด</option>
+    <option value="1">One</option>
+    <option value="2">Two</option>
+    <option value="3">Three</option>
+  </select>
+
+    <div class="row row-cols-1 row-cols-md-4 g-4" style="margin-top: 5%">
         @foreach($users as $user)
-            <div class="col-md-4 mb-4">
-                <div class="card h-100">
+            <div class="col mb-4">
+                <div class="card h-100" style="width: 286px;">
+                    <img src="images/login_bg.jpg" class="card-img-top" alt="ภาพ" style="width: 286px; height: 286px; object-fit: cover;">
                     <div class="card-body">
-                        <h5 class="card-title">{{ $user->userFirstname }} {{ $user->userSurname }}</h5>
-                        <p class="card-text"><strong>ประเภทผู้ใช้:</strong> {{ ucfirst($user->userType) }}</p>
+                        <h4 class="card-title"><strong>{{ $user->userFirstname }} {{ $user->userSurname }}</strong></h4>
+                        <p class="card-text"><strong>ตำแหน่ง</strong></p>
+                        <p class="card-text"><strong>จังหวัดที่สนใจ :</strong></p>
+                        <button type="button" class="btn btn-secondary" disabled>{{ ucfirst($user->userType) }}</button>
                         <a href="{{ route('user.show', $user->userID) }}" class="btn btn-primary">ดูรายละเอียด</a>
                     </div>
                 </div>
---
+++ resume-online/resources/views/layouts/company.blade.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <style>
+        body {
+            background-image: url('images/homie.svg'); /* ลิงก์ไปยังรูปภาพ */
+            background-size: cover; /* ทำให้รูปภาพครอบคลุมทั้งหน้าจอ */
+            background-position: center; /* จัดกึ่งกลางรูปภาพ */
+            background-repeat: no-repeat; /* ไม่ให้รูปภาพซ้ำ */
+            background-attachment: fixed;
+        }
+    </style>
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="/public/css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg" style="background-color: #efefef;">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">JOBIE</a>
+            <ul class="navbar-nav">
+                
+              <li class="nav-item">
+                <a class="nav-link" href="{{route('findUser')}}">ค้นหาผู้หางาน</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="{{route('findUser')}}">ค้นหานักศึกษาฝึกงาน</a>
+              </li>
+            </ul>
+              <ul class="navbar-nav ms-auto">
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <img src="images/02.svg" class="pic">
+                    <style>
+                        .pic{
+                            width: 30px;
+                            height: 30px;
+                            border-radius: 50%;
+                            object-fit: cover;
+                        }
+                    </style>
+                  </a>
+                  <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">ภาพรวม</a></li>
+                    <li><a class="dropdown-item" href="#">ตั้งค่าข้อมูลบริษัท</a></li>
+                    <li><a class="dropdown-item" href="#">ผู้สมัครงาน</a></li>
+                    <li><a class="dropdown-item" href="/">ออกจากระบบ</a></li>
+                    <style>
+                        .dropdown-menu{
+                            margin-left: -150%;
+                        }
+                    </style>
+                  </ul>
+                </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <div class="container py-2">
+            @yield('content')
+      </div>
+
+      <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
