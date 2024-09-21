0d03b0f312b6d75345c5fb20c8cc855980758c2c
Poomdevkub <sorawit.3p@gmail.com>
2024-09-21 23:47:47 +0700

    แก้หน้า user-dash

--- resume-online/resources/views/layout.blade.php
+++ resume-online/resources/views/layout.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title') | JOBIE </title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-dyQwq4jN1jxptpDDHAxtZwU4y3XpZG9wCx/q27P4kRF9q9RWVh7bR9QYtu8/c9av/0wHZotQsCX3QGlRf6EbZA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
 <body>
